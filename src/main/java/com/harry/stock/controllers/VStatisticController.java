d497c705d96399af5bacdcf052b91677e2c55815
LohaSolitaire <99732842+LohaSolitaire@users.noreply.github.com>
2023-11-21 20:18:10 +0300

    gg

--- src/main/java/com/harry/stock/controllers/VStatisticController.java
+++ src/main/java/com/harry/stock/controllers/VStatisticController.java
@@ -3,6 +3,7 @@ package com.harry.stock.controllers;
 import com.harry.stock.models.V_statistic;
 import com.harry.stock.repositories.VStatisticRepos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/statistics")
+@CrossOrigin(origins = "*")
 public class VStatisticController {
     @Autowired
     private VStatisticRepos vStatisticRepos;
